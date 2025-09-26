226e5696ffc98256215cf601e1066fa84bbdc82a
Gabriel/Korver <gabrielkorver76ers@gmail.com>
2025-09-26 18:51:52 -0300

    ajustes nas telas de login e header

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import Style from "./Login.module.css";
 import devImage from "../../assets/img.login2.jpg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [mostrarSenha, setMostrarSenha] = useState(false);
 
-  const logar = () => {
-    if (email === "mayara@dev.com" && senha === "123456") {
-      alert("Logado com sucesso!");
-      window.location.href = "/home";
-    } else {
-      alert("Email ou senha incorretos!");
+  const logar = async () => {
+    try {
+      const URL = "https://api-cadastro-backtend.onrender.com/usuario";
+      const response = await fetch(URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, senha }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem("usuario", JSON.stringify(data));
+        toast.success("Login realizado com sucesso!");
+        setTimeout(() => {
+          window.location.href = "/home";
+        }, 2000);
+      } else {
+        toast.error(data.error || "Credenciais inválidas!");
+      }
+    } catch (error) {
+      toast.error("Erro ao conectar com o servidor");
+      console.error(error);
     }
   };
 
@@ -34,15 +55,27 @@ const Login = () => {
             onChange={(e) => setEmail(e.target.value)}
             id="nome"
             placeholder="Digite seu nome"
+            className={Style.inputPadrao}
           />
 
+          {/* Input Senha */}
           <label htmlFor="senha">Senha</label>
-          <input
-            type="password"
-            onChange={(e) => setSenha(e.target.value)}
-            id="senha"
-            placeholder="Digite sua senha"
-          />
+          <div className={Style.inputSenha}>
+            <input
+              type={mostrarSenha ? "text" : "password"}
+              onChange={(e) => setSenha(e.target.value)}
+              id="senha"
+              placeholder="Digite sua senha"
+              className={Style.inputPadrao}
+            />
+            <span
+              className={Style.toggleSenha}
+              onClick={() => setMostrarSenha(!mostrarSenha)}
+            >
+              {mostrarSenha ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+            </span>
+          </div>
+
           <a href="/recuperar">Esqueceu a Senha?</a>
 
           <button onClick={logar}>Logar</button>
@@ -52,6 +85,19 @@ const Login = () => {
           </p>
         </div>
       </div>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,45 +1,89 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Style from "../Header/Header.module.css";
 import { FiLogOut, FiUserPlus, FiMenu, FiX } from "react-icons/fi";
 import { LuMessageSquarePlus } from "react-icons/lu";
 import { FaHome } from "react-icons/fa";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [usuarioLogado, setUsuarioLogado] = useState(null);
 
   function toggleMenu() {
     setMenuOpen(!menuOpen);
   }
 
+  function logout() {
+    localStorage.removeItem("usuario");
+    window.location.href = "/"; // redireciona para login
+  }
+
+  useEffect(() => {
+    const usuario = JSON.parse(localStorage.getItem("usuario"));
+
+    if (usuario) {
+      setUsuarioLogado(usuario);
+    } else {
+      // Exibe o toast e aguarda 1.5s antes de redirecionar
+      toast.error("Você precisa estar logado para acessar o feed", {
+        autoClose: 3000,
+      });
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 3000);
+    }
+  }, []);
+
   return (
-    <header className={Style.container}>
-      <div className={Style.boxFeed}>
-        <h1 className={Style.logo}>Dev Network</h1>
-        <p className={Style.textIcon}>
-          <FaHome className={Style.icon} />
-          Feed
-        </p>
-      </div>
+    <>
+      <header className={Style.container}>
+        <div className={Style.boxFeed}>
+          <h1 className={Style.logo}>Dev Network</h1>
+          <p className={Style.textIcon}>
+            <FaHome className={Style.icon} />
+            Feed
+          </p>
+        </div>
 
-      <button className={Style.menuButton} onClick={toggleMenu}>
-        {menuOpen ? <FiX size={28} /> : <FiMenu size={28} />}
-      </button>
+        <button className={Style.menuButton} onClick={toggleMenu}>
+          {menuOpen ? <FiX size={28} /> : <FiMenu size={28} />}
+        </button>
 
-      <div className={`${Style.boxOptions} ${menuOpen ? Style.showMenu : ""}`}>
-        <p className={Style.textIcon}>
-          <LuMessageSquarePlus className={Style.icon} />
-          Nova pergunta
-        </p>
-        <p className={Style.textIcon}>
-          <FiUserPlus className={Style.icon} />
-          Adicionar Dev
-        </p>
-        <p className={Style.textIcon}>
-          <FiLogOut className={Style.icon} />
-          Sair
-        </p>
-      </div>
-    </header>
+        <div
+          className={`${Style.boxOptions} ${menuOpen ? Style.showMenu : ""}`}
+        >
+          <p className={Style.textIcon}>
+            <LuMessageSquarePlus className={Style.icon} />
+            Nova pergunta
+          </p>
+          <p className={Style.textIcon}>
+            <FiUserPlus className={Style.icon} />
+            Adicionar Dev
+          </p>
+          <p>
+            Bem-vindo:{" "}
+            {usuarioLogado ? usuarioLogado.name || usuarioLogado.email : ""}
+          </p>
+          <p className={Style.textIcon} onClick={logout}>
+            <FiLogOut className={Style.icon} />
+            Sair
+          </p>
+        </div>
+      </header>
+
+      <ToastContainer
+        position="top-right"
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 };
 
