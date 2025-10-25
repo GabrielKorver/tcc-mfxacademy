0a693906073bf56494215de15030f9c75b03f2b9
MayCenteno <mayaracenteno1@gmail.com>
2025-10-25 20:10:10 -0300

    finalizacao do componente filtro

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,16 +3,20 @@ import Header from "../../components/Header/Header";
 import Perfil from "../../components/Perfil/Perfil";
 import { Feed } from "../../components/Feed/Feed";
 import Footer from "../../components/Footer/Footer";
+import Filtros from "../../components/Filtros/FiltrosPerguntas"  
 
 const Home = () => {
   return (
     <>
       <Header />
+      
       <div className={Style.container}>
         <div className={Style.container_left}>
           <Perfil />
         </div>
+        
         <div className={Style.container_right}>
+          <Filtros />
           <Feed />
           <Feed />
           <Feed />
---
+++ src/components/Filtros/FiltrosPerguntas.jsx
@@ -0,0 +1,25 @@
+import styles from "../Filtros/FiltrosPerguntas.module.css";
+import { FiSearch } from "react-icons/fi";
+
+function FiltrosPerguntas() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.filtros}>
+        <div className={styles.inputContainer}>
+          <FiSearch className={styles.icon} />
+          <input type="text" placeholder="Buscar perguntas..." />
+        </div>
+
+        <select name="Select tech" id="Select Tech">
+          <option value="todas">Todas as tecnologias</option>
+          <option value="react">React</option>
+          <option value="javascript">Javascript</option>
+          <option value="css">CSS</option>
+          <option value="html">HTML</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default FiltrosPerguntas;
